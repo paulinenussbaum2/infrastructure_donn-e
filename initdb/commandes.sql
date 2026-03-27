cfbc26b3e8cec404bb4b447eea034930da4c6713
Amel Bellal <amelbellal@hotmail.com>
2026-03-27 11:54:21 +0100

    normalisation

--- initdb/commandes.sql
+++ initdb/commandes.sql
@@ -1,29 +1,38 @@
+INSERT INTO type_materiel (libelle)
 SELECT DISTINCT (
-    CASE when type_materiel LIKE '%anc%' then 'banc'
-    when type_materiel LIKE '%ampadaire%' then 'lampadaire'
-    when type_materiel lIKE '%oubelle%' then 'poubelle'
-    when type_materiel lIKE 'corbeille%' then 'poubelle'
-    when type_materiel lIKE '%ontaine%' then 'fontaine'
-    when type_materiel lIKE '%orne%' then 'borne recharge'
-    when type_materiel lIKE '%anneau%' then 'panneau'
+    CASE 
+    WHEN LOWER (TRIM(type_materiel)) LIKE '%banc%' then 'banc'
+    WHEN LOWER (TRIM(type_materiel)) LIKE '%lampadaire%' then 'lampadaire'
+    WHEN LOWER (TRIM(type_materiel)) LIKE '%corbeille%' then 'poubelle'
+    WHEN LOWER (TRIM(type_materiel)) LIKE '%poubelle%' then 'poubelle'
+    WHEN LOWER (TRIM(type_materiel)) LIKE '%fontaine%' then 'fontaine'
+    WHEN LOWER (TRIM(type_materiel)) LIKE '%borne%' then 'borne recharge'
+    WHEN LOWER (TRIM(type_materiel)) LIKE '%panneau%' then 'panneau'
     ELSE NULL
     END
-    )
-FROM staging.inventaire_mobilier;
+)
+FROM staging.inventaire_mobilier
+WHERE type_materiel IS NOT NULL;
 
-SELECT DISTINCT (
-    CASE when type_materiau LIKE '%ois%' then 'bois'
-    when type_materiau LIKE '%étal%' then 'metal'
-    when type_materiau LIKE '%etal%' then 'metal'
-    when type_materiau LIKE '%odium%' then 'sodium'
-    when type_materiau LIKE 'LED%' then 'LED'
-    when type_materiau LIKE 'led%' then 'LED'
-    when type_materiau LIKE '%ierre%' then 'pierre'
-    when type_materiau LIKE '%éton%' then 'pierre'
-    ELSE NULL
-    END
-    ), type_materiau
-FROM staging.inventaire_mobilier;
+
+INSERT INTO type_materiau (libelle)
+SELECT *
+from (
+SELECT DISTINCT 
+CASE 
+WHEN LOWER (TRIM(type_materiau)) LIKE '%bois%' then 'bois'
+WHEN LOWER (TRIM(type_materiau)) LIKE '%métal%' then 'métal'
+WHEN LOWER (TRIM(type_materiau)) LIKE '%metal%' then 'métal'
+WHEN LOWER (TRIM(type_materiau)) LIKE '%sodium%' then 'sodium'
+WHEN LOWER (TRIM(type_materiau)) LIKE '%LED%' then 'LED'
+WHEN LOWER (TRIM(type_materiau)) LIKE '%pierre%' then 'pierre'
+WHEN LOWER (TRIM(type_materiau)) LIKE '%béton%' then 'béton'
+END as type_cleaned
+FROM staging.inventaire_mobilier
+)
+WHERE type_cleaned IS NOT NULL;
+
+
 
 SELECT DISTINCT (
     CASE
@@ -36,11 +45,52 @@ END
 )
 FROM staging.inventaire_mobilier;
 
-SELECT DISTINCT (
-    CASE when etat LIKE 'usé' then 'usé'
-    when etat LIKE 'bon' then 'bon'
-    when etat LIKE 'à remplacer' then 'à remplacer'
+INSERT INTO etat (libelle)
+SELECT DISTINCT 
+    CASE 
+    WHEN LOWER (TRIM(etat)) LIKE '%usé%' then 'usé'
+    WHEN LOWER (TRIM(etat)) LIKE '%bon%' then 'bon'
+    WHEN LOWER (TRIM(etat)) LIKE '%à remplacer%' then 'à remplacer'
     ELSE NULL
     END
-    )
-FROM staging.inventaire_mobilier;
+FROM staging.inventaire_mobilier
+WHERE etat IS NOT NULL;
+
+
+INSERT INTO statut (libelle)
+SELECT DISTINCT
+    CASE 
+    WHEN LOWER (TRIM(statut)) LIKE '%en attente%' then 'en cours'
+    WHEN LOWER (TRIM(statut)) LIKE '%en cours%' then 'en cours'
+    WHEN LOWER (TRIM(statut)) LIKE '%fait%' then 'fait'
+    ELSE NULL
+    END
+FROM staging.signalements
+WHERE statut IS NOT NULL;
+
+
+INSERT INTO type_intervention (libelle)
+SELECT DISTINCT 
+CASE 
+WHEN LOWER (TRIM(type_intervention)) LIKE '%peinture%' then 'peinture'
+WHEN LOWER (TRIM(type_intervention)) LIKE '%réparation%' then 'réparation'
+WHEN LOWER (TRIM(type_intervention)) LIKE '%remise en service%' then 'réparation'
+WHEN LOWER (TRIM(type_intervention)) LIKE '%remplacement%' then 'remplacement'
+WHEN LOWER (TRIM(type_intervention)) LIKE '%réparation électrique%' then 'réparation'
+WHEN LOWER (TRIM(type_intervention)) LIKE '%réparation fuite%' then 'réparation'
+WHEN LOWER (TRIM(type_intervention)) LIKE '%réparation vitre%' then 'réparation'
+WHEN LOWER (TRIM(type_intervention)) LIKE '%nettoyage%' then 'nettoyage'
+WHEN LOWER (TRIM(type_intervention)) LIKE '%nettoyage tags%' then 'nettoyage'
+WHEN LOWER (TRIM(type_intervention)) LIKE '%hivernage%' then 'hivernage'
+WHEN LOWER (TRIM(type_intervention)) LIKE '%redressage mât%' then 'réparation'
+WHEN LOWER (TRIM(type_intervention)) LIKE '%détartrage%' then 'détartrage'
+WHEN LOWER (TRIM(type_intervention)) LIKE '%mise à jour logiciel%' then 'mise à jour logiciel'
+ELSE NULL
+    END
+FROM staging.interventions
+WHERE type_intervention IS NOT NULL;
+
+
+SELECT DISTINCT 
+CASE 
+WHEN LOWER (TRIM ())
